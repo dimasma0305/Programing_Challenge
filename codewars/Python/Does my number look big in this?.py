b426aa7abb31f8e972227dd116ec341f84f8152b
dimasma0305 <dimasmaulana0305@gmail.com>
2022-03-15 07:07:04 +0800

    07:06:50_15/03/22

---
+++ codewars/Python/Does my number look big in this?.py
@@ -0,0 +1,17 @@
+import re
+
+
+def narcissistic( value ):
+    # pisahkan value menjadi list
+    value_now = [int(i) for i in list(str(value))]
+    total = 0
+    for i in value_now:
+        total += i ** len(value_now)
+    if total == value:
+        return True
+    else:
+        return False
+        
+
+if __name__ == '__main__':
+    print(narcissistic( 371 ))
