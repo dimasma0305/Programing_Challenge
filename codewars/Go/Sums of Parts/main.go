7018b346a10ad7e1fda226cf64fc464ec88588ce
dimas0305 <dimasmaulana0305@gmail.com>
2022-06-22 23:23:37 +0800

    Wed Jun 22 03:23:37 PM UTC 2022

--- codewars/Go/Sums of Parts/main.go
+++ codewars/Go/Sums of Parts/main.go
@@ -3,19 +3,28 @@ package main
 import (
 	"fmt"
 )
+
 func reverseInts(input []uint64) []uint64 {
-    if len(input) == 0 {
-        return input
-    }
-    return append(reverseInts(input[1:]), input[0]) 
+	if len(input) == 0 {
+		return input
+	}
+	return append(reverseInts(input[1:]), input[0])
 }
 
 func PartsSums(ls []uint64) []uint64 {
 	sum := make([]uint64, len(ls)+1)
-	for i, e := 0, reverseInts(ls); i< len(ls);i++{
-		sum[len(ls) - i - 1] += sum[len(ls) - i] + e[i]
-	} 
+	for i, e := 0, reverseInts(ls); i < len(ls); i++ {
+		sum[len(ls)-i-1] += sum[len(ls)-i] + e[i]
+	}
 	return sum
+}
+
+func PartsSums2(ls []uint64) []uint64 {
+	v := make([]uint64, len(ls)+1)
+	for i := len(ls) - 1; i >= 0; i-- {
+		v[i] = v[i+1] + ls[i]
+	}
+	return v
 }
 
 func main() {
