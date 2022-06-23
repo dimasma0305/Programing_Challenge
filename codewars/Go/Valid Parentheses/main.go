7a046974f8b9690937326a5403a7594822ff9a16
dimas0305 <dimasmaulana0305@gmail.com>
2022-06-23 21:00:01 +0800

    Thu Jun 23 01:00:01 PM UTC 2022

--- codewars/Go/Valid Parentheses/main.go
+++ codewars/Go/Valid Parentheses/main.go
@@ -1,20 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 func ValidParentheses(parens string) bool {
-	if len(parens) % 2 != 0 {
+	if len(parens)%2 != 0 {
 		return false
 	}
-	dict := map[string]string{"(":")"}
+	dict := map[string]string{"(": ")"}
 	var stack []string
 	for _, i := range parens {
-		i = string(i)
-		if i == dict["("] {
 
+		if _, ok := dict[string(i)]; ok {
+			stack = append(stack, string(i))
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+			a := stack[len(stack)-1]
+			stack = append(stack[:len(stack)-1], stack[len(stack)+1-1:]...)
+			if string(i) != dict[a] {
+				return false
+			}
 		}
 	}
 
+	return len(stack) == 0
+}
+func ValidParentheses(parens string) bool {
+	b := 0
+	for _, c := range parens {
+		if c == '(' {
+			b++
+		} else {
+			b--
+			if b < 0 {
+				return false
+			}
+		}
+	}
+	return b == 0
+}
+func ValidParentheses(parens string) bool {
+	_, err := regexp.Compile(parens)
+	return err == nil
+}
+func ValidParentheses(parens string) bool {
+	for strings.Contains(parens, "()") {
+		parens = strings.Replace(parens, "()", "", -1)
+	}
+	return len(parens) == 0
 }
 
 func main() {
-
+	fmt.Println(ValidParentheses("(())((()((()))))"))
 }
