ad9962102d7c0702b50a2a3011399feaad7d2c1a
dimas0305 <dimasmaulana0305@gmail.com>
2022-06-24 23:24:54 +0800

    Wed Jun 29 12:42:08 AM UTC 2022

--- codewars/Go/Directions Reduction/main.go
+++ codewars/Go/Directions Reduction/main.go
@@ -5,50 +5,24 @@ import (
 )
 
 func DirReduc(arr []string) []string {
-	var (
-		n int
-		e int
-		s int
-		w int
-	)
-	for i := 0; i < len(arr); i++ {
-		switch arr[i] {
-		case "NORTH":
-			n += 1
-		case "EAST":
-			e += 1
-		case "SOUTH":
-			s += 1
-		case "WEST":
-			w += 1
+	dict := map[string]string{
+		"NORTH": "SOUTH",
+		"EAST":  "WEST",
+		"SOUTH": "NORTH",
+		"WEST":  "EAST",
+	}
+	res := []string{}
+	for _, i := range arr {
+		if len(res) != 0 && dict[i] == res[len(res)-1] {
+			res = res[:len(res)-1]
+		} else {
+			res = append(res, i)
 		}
-	}
-	NminS := n - s
-	EminW := e - w
-	newArr := []string{}
-	if NminS < 0 {
-		newArr = arrAppend(newArr, "SOUTH", NminS*NminS)
-	}
-	if NminS > 0 {
-		newArr = arrAppend(newArr, "NORTH", NminS)
-	}
-	if EminW < 0 {
-		newArr = arrAppend(newArr, "WEST", EminW*EminW)
-	}
-	if EminW > 0 {
-		newArr = arrAppend(newArr, "EAST", NminS)
-	}
-	return newArr
 
-}
-func arrAppend(arr []string, txt string, dup int) []string {
-	var n []string
-	for i := 0; i < dup; i++ {
-		n = append(n, txt)
 	}
-	return n
+	return res
 }
 
 func main() {
-	fmt.Println(DirReduc([]string{}))
+	fmt.Println(DirReduc([]string{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}))
 }
