1f96ebd883a43f7099c0d8c4d9b8421936ba8cc1
dimas0305 <dimasmaulana0305@gmail.com>
2022-06-29 18:00:01 +0800

    Wed Jun 29 10:00:01 AM UTC 2022

---
+++ codewars/Go/ROT13/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Rot13(msg string) string {
+	var enc []string
+	for _, i := range msg {
+		if i >= 'a' && i <= 'z' {
+			i += 13
+			if i > 'z' {
+				i -= 26
+			}
+		}
+		if i >= 'A' && i <= 'Z' {
+			i += 13
+			if i > 'Z' {
+				i -= 26
+			}
+		}
+		enc = append(enc, string(i))
+	}
+	return strings.Join(enc, "")
+}
+func main() {
+	fmt.Println(Rot13("dimas maulana"))
+}
