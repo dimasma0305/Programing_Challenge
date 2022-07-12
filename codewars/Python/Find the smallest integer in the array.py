5fa2e4a186ed202233cc862d25f38e06a6208dba
dimas0305 <dimasmaulana0305@gmail.com>
2022-07-12 18:00:01 +0800

    Tue Jul 12 10:00:01 AM UTC 2022

---
+++ codewars/Python/Find the smallest integer in the array.py
@@ -0,0 +1,6 @@
+def find_smallest_int(arr):
+    # Code here
+    arr = min(arr)
+    return arr
+
+print(find_smallest_int([1,2,3,4, -123]))
---
+++ codewars/Python/Grasshopper - Summation.py
@@ -0,0 +1,10 @@
+def summation(num):
+    n = 0
+    for i in range(1, num+1):
+        n += i
+    return n 
+
+def summation(num):
+    return sum(range(num + 1))
+
+print(summation(2))
