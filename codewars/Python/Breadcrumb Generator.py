77b9a0ac38fe162f0997c25b499dd1e1b8cd4296
dimas0305 <dimasmaulana0305@gmail.com>
2022-07-12 12:00:01 +0800

    Tue Jul 12 04:00:01 AM UTC 2022

--- codewars/Python/Breadcrumb Generator.py
+++ codewars/Python/Breadcrumb Generator.py
@@ -1,20 +1,47 @@
-import html
-
+import re
 def generate_bc(url, separator):
-    parse_url = url.split('/')
+    # print(url)
+    # replace http or https with '' and remove #.*
+    ignore = ["the","of","in","from","by","with","and", "or", "for", "to", "at", "a"]
+    parse_url = re.sub(r'(http|https)://', '', url)
+    parse_url = re.sub(r'#.*', '', parse_url)
+    parse_url = re.sub(r'\?.*', '', parse_url)
+    # print(parse_url)
+    parse_url = parse_url.split('/')
     parse_url[-1] = parse_url[-1].split('?')[0]
     parse_url = [i for i in parse_url if i != '']
+    if len(parse_url) < 2:
+        return '<span class="active">HOME</span>'
     parse_url.pop(0)
+    file = parse_url[-1].split('.')[0]
     path = parse_url[:-1]
-    file = parse_url[-1]
-    # print(path)
+    
     # print(file)
+    if file == 'index':
+        try:
+            file = parse_url[-2]
+            path = parse_url[:-2]
+        except:
+            return '<span class="active">HOME</span>'
+    # print(file)
+    
+    
     breadcrumb = """<a href="/">HOME</a>%s""" % separator
-    for i, c in enumerate(path):
-        breadcrumb += """<a href="/%s/">%s</a>%s""" % (c, c.upper(), separator)
-    if file != '' and file != file.split('.')[0]:
-        breadcrumb += """<span class="active">%s</span>""" % file.split('.')[0].upper()
+    plus = []
+    for _, c in enumerate(path):
+        plus.append(c)
+        if len(c) > 30:
+            c = [i for i in c.split('-')]
+            c = ''.join([i[0] for i in c if i not in ignore])
+        breadcrumb += """<a href="/%s/">%s</a>%s""" % ('/'.join(plus), c.upper().replace('-', ' '), separator)
+    file = file.split('.')[0].replace("-", " ")
+    # print(file)
+    if len(file) > 30:
+        file = [i for i in file.split(' ')]
+        # print(file)
+        file = ''.join([i[0] for i in file if i not in ignore])
+    breadcrumb += """<span class="active">%s</span>""" % file.upper()
     # return html.escape(breadcrumb)
     return breadcrumb
     
-print(generate_bc('mysite.com/pictures/holidays.html?testing', ':'))
+print(generate_bc('http://twitter.de/issues/kamehameha-kamehameha-the-paper-kamehameha/bed-and-paper-and-meningitis-immunity', ':'))
