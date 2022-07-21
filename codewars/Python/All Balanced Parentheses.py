60d60209f096d8cb07823477c2f7dad98355654f
dimas0305 <dimasmaulana0305@gmail.com>
2022-07-21 21:00:01 +0800

    Thu Jul 21 01:00:01 PM UTC 2022

--- codewars/Python/All Balanced Parentheses.py
+++ codewars/Python/All Balanced Parentheses.py
@@ -1,30 +1,30 @@
 from itertools import combinations
+import cProfile
 
 class ballanced_parentheses:
     def __init__(self, n: int) -> None:
         self.brackets = ['(', ')']
         self.n = n*2
-        self.gen_brackets = self.__gen_brackets()
         self.combination_brackets = self.__bracket_combinations()
 
-    def __gen_brackets(self) -> list:
-        return [i for i in self.brackets]*self.n
-
     def __bracket_combinations(self) -> list:
-            '''generate an unique brackets combination
-            that distincts from each other'''
-            return list(combinations(self.gen_brackets, self.n))
+            n = set(combinations([i for i in self.brackets]*self.n, self.n))
+            lst = []
+            for i in n:
+                if self.check_validity(i):
+                    lst += [i]
+            return lst
         
     def check_validity(self, brackets: list) -> bool:
         stack = []
         for i in brackets:
             if i == "(":
-                stack.append(i)
+                stack += [i]
             elif i in ")":
                 if len(stack) == 0:
                     return False
                 else:
-                    stack.pop()
+                    stack = stack[:-1]
         if len(stack) == 0:
             return True
         else:
@@ -33,8 +33,10 @@ class ballanced_parentheses:
     def get_brackets(self) -> list:
         if self.n == 0:
             return ['']
-        return [i for i in self.combination_brackets]
+        return [''.join(i) for i in self.combination_brackets]
+
 
 
 balanced_parens = lambda n: ballanced_parentheses(n).get_brackets()
-print(balanced_parens(3))
+print(balanced_parens(1))
+# cProfile.run('balanced_parens(7)')
